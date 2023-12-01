9c47419bd5cc52affcf1f3ba737f2ae6312bb2f8
Marcin <marcinkrueger@onet.eu>
2023-12-02 00:47:04 +0100

    Add dataset class with cifar10 downloading

---
+++ src/dataset.py
@@ -0,0 +1,18 @@
+import os
+import torchvision.datasets as datasets
+
+class Dataset:
+    def __init__(self, replace = False):
+        self.path = "../data"
+        self.replace = replace
+        self.dataset = self.create_dataset()
+        print(self.dataset)
+
+    def create_dataset(self):
+        if self.replace or not os.path.exists(self.path) or not len(os.listdir(self.path)) > 0:
+            return datasets.CIFAR10(root=self.path, train=False, download=True, transform=None)
+        else:
+            if os.path.exists("../data/cifar-10-batches-py"):
+                return datasets.CIFAR10(root=self.path, train=False, download=False, transform=None)
+            else:
+                raise "Corrupted folder"
