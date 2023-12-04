4e310abb9223bc494486a0b8d5b14b297bb956f6
Marcin <marcinkrueger@onet.eu>
2023-12-05 00:09:16 +0100

    Simplify train and test data split

--- src/dataset.py
+++ src/dataset.py
@@ -1,8 +1,6 @@
 import os
 
-from torch.utils.data import Subset
 from torchvision import datasets
-from sklearn.model_selection import train_test_split
 
 
 class Dataset:
@@ -11,28 +9,16 @@ class Dataset:
         "fashion": {"directory": "FashionMNIST", "instance": datasets.FashionMNIST},
     }
 
-    def __init__(self, type="cifar", test_size=0.2):
+    def __init__(self, type="cifar"):
         self.path = "../data"
         self.dataset_info = Dataset.types[type]
         self.dataset_path = f"{self.path}/{self.dataset_info['directory']}"
-        self.test_size = test_size
 
-        self.dataset = self.create_dataset()
-        self.train_dataset, self.test_dataset = self.dataset_split()
+        self.train_dataset = self.create_dataset(True)
+        self.test_dataset = self.create_dataset(False)
 
-        print(len(self.train_dataset))
-        print(len(self.test_dataset))
-
-    def create_dataset(self):
+    def create_dataset(self, train):
         return self.dataset_info["instance"](
-            root=self.path, train=False, download=True, transform=None
+            root=self.path, train=train, download=True, transform=None
         )
-
-    def dataset_split(self):
-        train_indices, test_indices = train_test_split(
-            range(len(self.dataset)), test_size=self.test_size
-        )
-        train = Subset(self.dataset, train_indices)
-        test = Subset(self.dataset, test_indices)
-
-        return train, test
+    
