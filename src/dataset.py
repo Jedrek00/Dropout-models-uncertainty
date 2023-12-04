e8cb989dcfa4de2e0552c6571065e05e5ab345f3
Marcin <marcinkrueger@onet.eu>
2023-12-04 23:18:30 +0100

    Add scikit-learn + fashin-mnist into Dataset class

--- src/dataset.py
+++ src/dataset.py
@@ -1,27 +1,38 @@
 import os
-import torchvision.datasets as datasets
+
+from torch.utils.data import Subset
+from torchvision import datasets
+from sklearn.model_selection import train_test_split
 
 
 class Dataset:
-    def __init__(self, replace=False):
+    types = {
+        "cifar": {"directory": "cifar-10-batches-py", "instance": datasets.CIFAR10},
+        "fashion": {"directory": "FashionMNIST", "instance": datasets.FashionMNIST},
+    }
+
+    def __init__(self, type="cifar", test_size=0.2):
         self.path = "../data"
-        self.replace = replace
+        self.dataset_info = Dataset.types[type]
+        self.dataset_path = f"{self.path}/{self.dataset_info['directory']}"
+        self.test_size = test_size
+
         self.dataset = self.create_dataset()
-        print(self.dataset)
+        self.train_dataset, self.test_dataset = self.dataset_split()
+
+        print(len(self.train_dataset))
+        print(len(self.test_dataset))
 
     def create_dataset(self):
-        if (
-            self.replace
-            or not os.path.exists(self.path)
-            or not len(os.listdir(self.path)) > 0
-        ):
-            return datasets.CIFAR10(
-                root=self.path, train=False, download=True, transform=None
-            )
-        else:
-            if os.path.exists("../data/cifar-10-batches-py"):
-                return datasets.CIFAR10(
-                    root=self.path, train=False, download=False, transform=None
-                )
-            else:
-                raise "Corrupted folder"
+        return self.dataset_info["instance"](
+            root=self.path, train=False, download=True, transform=None
+        )
+
+    def dataset_split(self):
+        train_indices, test_indices = train_test_split(
+            range(len(self.dataset)), test_size=self.test_size
+        )
+        train = Subset(self.dataset, train_indices)
+        test = Subset(self.dataset, test_indices)
+
+        return train, test
