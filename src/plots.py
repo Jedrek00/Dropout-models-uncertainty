a60ef438cf11768b109026d7694dfb8658a915b7
Marcin <marcinkrueger@gmail.com>
2023-12-13 12:26:28 +0100

    Create uncertainty plot function

--- src/plots.py
+++ src/plots.py
@@ -1,7 +1,11 @@
 from typing import Optional
 
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+
+import matplotlib.image as mpimg
 
 
 def plot_history(history: dict, filepath: Optional[str] = None):
@@ -29,3 +33,55 @@ def plot_confusion_matrix(preds: list, labels: list, class_names: list, filepath
     plt.tight_layout()
     if filepath is not None:
         plt.savefig(filepath)
+
+# input: (100, 10)
+def plot_uncertainty(probs: list, max_n: Optional[int] = None, img_path: Optional[str] = None, img_size: int = 32, filepath: Optional[str] = None):
+    CONST_I = 1
+    max_n = max_n if max_n else 10
+    best_n = list(range(10))
+
+    if max_n != None:
+        sums = np.argsort(np.sum(probs.T, axis=1))[::-1]
+        best_n = sums[:max_n]
+
+    x = [CONST_I] * probs.shape[0]
+    colors = plt.cm.viridis(np.linspace(0, 1, max_n))
+    legend_handles = []
+
+    new_probs = probs.T[best_n, :]
+    for val, p, i in zip(best_n, new_probs, list(range(max_n))):
+        scatter = plt.scatter(x, p, color=colors[i], s=50000, marker='_', alpha=0.3, label=f"value: {val + 1}")
+        legend_handles.append(scatter)
+
+    plt.ylim(0, 1)
+    plt.xlim(0,2)
+    plt.xticks([1], "")
+    if img_path != None:
+        img = plt.imread(img_path)
+        ax = plt.gca()
+        tick_labels = ax.xaxis.get_ticklabels()
+        ib = OffsetImage(img, zoom=img_size / img.shape[0])
+        ib.image.axes = ax
+        ab = AnnotationBbox(ib,
+                        tick_labels[0].get_position(),
+                        frameon=False,
+                        box_alignment=(0.5, 1.2)
+                        )
+        ax.add_artist(ab)
+    plt.ylabel("probability")
+    legend = plt.legend(handles=legend_handles)
+    for handle in legend.legend_handles:
+        handle.set_sizes([100])
+    
+    if filepath != None:
+        plt.savefig(filepath)
+    plt.show()
+
+
+def create_probs(n: int):
+    probs = []
+    for _ in range(n):
+        rand_probs = np.random.rand(10)
+        norm_probs = rand_probs / np.sum(rand_probs)
+        probs.append(norm_probs)
+    return np.array(probs)
